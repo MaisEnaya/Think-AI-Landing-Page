576fe1c370ab97bbf2fbb0c6193204028c603dc6
Mais <maisenaya@icloud.com>
2025-11-12 04:14:05 +0300

    hero draft

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Hero = () => {
     return (
-        <div>Hero</div>
+        <section id="hero">
+            <div>
+                <img
+                    src="/think-logo-white-glow.png"
+                    alt="Think AI Logo"
+                    className="h-9 md:h-10 w-auto"
+                />
+                <h1 className="text-2xl md:text-4xl font-light text-[#D9D9D9] max-w-4xl">
+                    World’s first Unified AI Compute Ecosystem for <br />Sovereign Intelligence
+                </h1>
+
+            </div>
+            </section>
     )
 }
 export default Hero
